aa65d4583c74c81c63c8fe30a9644a202113afc5
anand.sonwane123 <anand.sonwane123@gmail.com>
2025-08-04 02:49:31 +0000

    Initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,26 +2,45 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
-  @Value("${NAME:World}")
-  String name;
-
-  @RestController
-  class HelloworldController {
-    @GetMapping("/")
-    String hello() {
-      return "Hello " + name + "!";
-    }
-  }
-
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
+   
+    System.out.println("============================================");
+
+    factorial(5);
   }
+
+  public static void factorial(int a){
+		int fact = 1;
+		for(int i = 1; i <= a; i++ ){
+			fact *= i;
+		}
+
+		System.out.printf("Factorial of %d id %d.", a , fact);
+	}
+
+	public static void contertToBinary(int a){
+		System.out.println(Integer.toBinaryString(a));
+	}
+
+	public static void addition(int a, int b){
+		int sum = a + b;
+
+		System.out.printf("Sum of %d, %d is %d.", a, b, sum);
+	}
+
+	public static void swapNumber(int a, int b){
+		System.out.printf("Before swaping numbers a: %d, b: %d ", a, b);
+
+		int c = a + b;
+		a = c - a;
+		b = c - b;
+		System.out.printf("After swaping numbers a: %d, b: %d ", a, b);
+
+	}
 
 }
